92f1b9b41cc9c52cdbb2ae4eb68ffae5ed0be5a4
michael-graute <m.graute@team-braeutigam.de>
2023-09-09 11:57:21 +0200

    added code for actually playing a note

--- src/app/midi-monitor/midi-monitor.component.ts
+++ src/app/midi-monitor/midi-monitor.component.ts
@@ -3,6 +3,7 @@ import {MIDI_MESSAGES} from "@ng-web-apis/midi";
 import {Observable} from "rxjs";
 import {map} from 'rxjs/operators';
 import * as Tone from "tone";
+import {SynthService} from "../synth.service";
 
 @Component({
   selector: 'ins-midi-monitor',
@@ -13,7 +14,7 @@ export class MidiMonitorComponent implements OnInit{
 
   readonly notes$: Observable<[number, number, number]>;
 
-  constructor(@Inject(MIDI_MESSAGES) messages$: Observable<MIDIMessageEvent>) {
+  constructor(@Inject(MIDI_MESSAGES) messages$: Observable<MIDIMessageEvent>, public synthService: SynthService) {
     this.notes$ = messages$.pipe(
       map((message: MIDIMessageEvent) => [message.data[0], message.data[1], message.data[2]])
     );
@@ -25,8 +26,11 @@ export class MidiMonitorComponent implements OnInit{
       if(note[0] === 144 && note[2] > 0) { // note on
         console.log('Note On', Tone.Frequency(note[1], "midi").toNote());
         console.log('Velocity', (1/127) * note[2]);
+        this.synthService.currentNote = Tone.Frequency(note[1], "midi").toNote();
+        this.synthService.play();
       } else if(note[0] === 144 && note[2] === 0) { // note off
         console.log('Note Off');
+        this.synthService.stop();
       }
     });
   }
