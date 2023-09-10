58649380c936d6cd30286106f5a1737160d0aaba
michael-graute <m.graute@team-braeutigam.de>
2023-09-10 22:25:24 +0200

    code cleanup and refactoring

--- src/app/keyboard/keyboard.component.ts
+++ src/app/keyboard/keyboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {SynthService} from "../synth.service";
 
 @Component({
@@ -6,20 +6,23 @@ import {SynthService} from "../synth.service";
   templateUrl: './keyboard.component.html',
   styleUrls: ['./keyboard.component.scss']
 })
-export class KeyboardComponent {
+export class KeyboardComponent implements OnInit {
   octaveBase : number = 0;
   public notes : string[] = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
   public octaves : number[] = [];
   public msDwn : boolean = false;
-  private lastNote : string = "";
+  public heldNote: string = "";
+  public lastNote : string = "";
+  public currentNote : string = "";
 
-  constructor(public synthService: SynthService) {
+  constructor(public synthService: SynthService, private changeDetectorRef: ChangeDetectorRef) {
     this.changeOctave(2)
   }
 
   play(note:string){
     this.synthService.currentNote = note;
     if(this.synthService.sequencerEnabled) {
+      this.synthService.sequencerRootNote = note;
       this.synthService.playSequence();
     } else {
       this.synthService.play();
@@ -28,6 +31,7 @@ export class KeyboardComponent {
 
   stop(){
     if(this.synthService.sequencerEnabled) {
+      this.synthService.sequencerRootNote = "C4";
       this.synthService.stopSequence();
     } else {
       this.synthService.stop();
@@ -38,10 +42,14 @@ export class KeyboardComponent {
     this.msDwn = true;
     this.play(note + octave);
     this.lastNote = note + octave;
+    this.heldNote = note + octave;
+    this.currentNote = note + octave;
   }
 
   mouseUp(){
     this.msDwn = false;
+    this.heldNote = "";
+    this.lastNote = "";
     this.stop();
   }
 
@@ -52,6 +60,7 @@ export class KeyboardComponent {
     if(this.msDwn) {
       if(this.synthService.sequencerEnabled && (note + octave) != this.lastNote) {
           this.synthService.currentNote = note + octave;
+          this.synthService.sequencerRootNote = note + octave;
       } else if(!this.synthService.sequencerEnabled) {
         this.play(note + octave);
       }
@@ -62,5 +71,12 @@ export class KeyboardComponent {
   changeOctave(octave:number){
     this.octaveBase = this.octaveBase + octave;
     this.octaves = [this.octaveBase + 1, this.octaveBase + 2, this.octaveBase + 3]
+  }
+
+  ngOnInit(): void {
+    this.synthService.noteOn.subscribe((note: string) => {
+      this.currentNote = note;
+      this.changeDetectorRef.detectChanges();
+    });
   }
 }
